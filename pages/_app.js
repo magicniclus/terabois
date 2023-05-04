55119836af43b097ae392e978c8fd61e3b3de0f0
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-04 11:18:06 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,27 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Provider } from "react-redux";
+import store from "../redux/store";
+import TagManager from "react-gtm-module";
+import { useEffect } from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const tagManagerArgs = {
+      gtmId: "GTM-K3X5GGQ",
+    };
+    TagManager.initialize(tagManagerArgs);
+  }, []);
+  return (
+    <>
+      <Head>
+        {/* <link rel="shortcut icon" href="/images/AdsVenture-favicon.png" /> */}
+      </Head>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
