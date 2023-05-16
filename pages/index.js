9d22e72d48ad58f98abeb0fe2b553797ef6aee87
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-16 12:34:02 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
+import Features from "../components/organisms/features/Features";
 import Basic from "../layouts/Basic";
 
 export default function Home() {
-  return <Basic></Basic>;
+  return (
+    <Basic>
+      <Features />
+    </Basic>
+  );
 }
