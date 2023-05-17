04129ae24ed75e0ae338dc228997dbb0538e2449
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-17 17:05:47 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -4,12 +4,14 @@ import Content from "../components/organisms/content/Content";
 import Faq from "../components/organisms/faq/Faq";
 import Testamonials from "../components/organisms/testamonial/Testamonials";
 import Cta from "../components/organisms/cta/Cta";
+import BlogContent from "../components/organisms/content/BlogContent";
 
 export default function Home() {
   return (
     <Basic>
       <Features />
       {/* <Content /> */}
+      <BlogContent />
       <Testamonials />
       <Cta />
     </Basic>
