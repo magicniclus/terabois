ea320b5120fdbb87cdba010713e6e92afbe960d0
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-17 15:08:12 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import Features from "../components/organisms/features/Features";
 import Basic from "../layouts/Basic";
+import Content from "../components/organisms/content/Content";
+import Faq from "../components/organisms/faq/Faq";
+import Testamonials from "../components/organisms/testamonial/Testamonials";
+import Cta from "../components/organisms/cta/Cta";
 
 export default function Home() {
   return (
     <Basic>
       <Features />
+      {/* <Content /> */}
+      <Testamonials />
+      <Cta />
     </Basic>
   );
 }
