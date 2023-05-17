c64cd85b5baf7e4cd9adca21ae1e99b6e74f12bb
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-17 15:08:04 +0200

    Update Features.js

--- components/organisms/features/Features.js
+++ components/organisms/features/Features.js
@@ -20,34 +20,25 @@ const prestations = [
     description:
       "Nous intervenons rapidement pour tout dépannage informatique.",
     href: "#",
-    icon: "./img/prestations/isolationcombles.jpg",
+    icon: "https://images.unsplash.com/photo-1603439810849-5e013dc3ce73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1476&q=80",
   },
   {
     name: "Terrasse bois",
     description:
       "Nous intervenons rapidement pour tout dépannage informatique.",
     href: "#",
-    icon: "./img/prestations/isolationcombles.jpg",
+    icon: "https://images.unsplash.com/photo-1603439810849-5e013dc3ce73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1476&q=80",
   },
 ];
 
 const Features = () => {
   return (
-    <div className="bg-white py-24 sm:py-32">
+    <div className="bg-white py-10">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:text-center">
-          <h2 className="mt-2 text-3xl font-bold tracking-tight text-gray-800 sm:text-4xl">
-            Vous souhaitez
-          </h2>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
-            Nos prestations sont adaptées à vos besoins. Nous vous proposons un
-            service de qualité, rapide et efficace.
-          </p>
-        </div>
         <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1">
           {prestations.map((prestation) => (
-            <div className="mt-24 grid grid-cols-1 gap-y-20 gap-x-6 sm:gap-x-8 md:w-5/6 mx-auto hover:bg-gray-50 p-2 rounded-xl">
-              <div className="group relative">
+            <div className="mt:-5 grid grid-cols-1 gap-y-20 gap-x-6 sm:gap-x-8 w-full mx-auto hover:bg-gray-50 p-7 rounded-xl">
+              <div className="group relative flex flex-col justify-between">
                 <div className="aspect-w-3 aspect-h-2 rounded-lg overflow-hidden sm:aspect-w-3 sm:aspect-h-4">
                   <img
                     src={prestation.icon}
@@ -55,15 +46,12 @@ const Features = () => {
                     className="object-cover pointer-events-none w-full h-full"
                   />
                 </div>
-                <h3 className="mt-6 text-sm text-gray-500">
+                <h3 className="mt-6 text-lg text-gray-800">
                   <a href={prestation.href}>
                     <span className="absolute inset-0" />
                     {prestation.name}
                   </a>
                 </h3>
-                <p className="mt-1 text-lg font-medium text-gray-800 mb-3">
-                  <a href={prestation.href}>{prestation.description}</a>
-                </p>
                 <a
                   href="#"
                   className="text-sm font-normal leading-6 text-gray-800"
