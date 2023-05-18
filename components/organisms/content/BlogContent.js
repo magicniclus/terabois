3f9a28d9655d27c8bdaf1bb0f01679edb1556f9d
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-18 10:19:42 +0200

    Update BlogContent.js

--- components/organisms/content/BlogContent.js
+++ components/organisms/content/BlogContent.js
@@ -2,16 +2,21 @@ import React from "react";
 
 const BlogContent = () => {
   return (
-    <div className="relative isolate bg-white  px-6 lg:px-8">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8 grid gap-y-4">
-        <div className="w-full bg-logoOne h-[300px] rounded-xl overflow-hidden relative">
+    <div className="relative isolate px-6 lg:px-8  pt-24 sm:pt-32">
+      <div className="mx-auto max-w-xl text-center">
+        <h2 className="mt-2 text-3xl font-bold tracking-tight text-gray-800 sm:text-4xl">
+          Toutes notre expertise en article
+        </h2>
+      </div>
+      <div className="mx-auto max-w-7xl p-6 lg:p-8 grid gap-y-4 rounded-xl  mt-16">
+        <div className="w-full h-[200px] rounded-xl overflow-hidden relative">
           <img
             src="https://images.unsplash.com/photo-1591684080176-bb2b73f9ec68?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80"
             alt="isolation des combles"
             className="object-cover pointer-events-none w-full h-full"
           />
           <div className="absolute inset-0 bg-gradient-to-t from-black opacity-50"></div>
-          <div className="absolute bottom-10 left-10 text-2xl lg:text-4xl">
+          <div className="absolute bottom-10 left-10 text-xl lg:text-2xl">
             <a href="#" className="text-white">
               <h3 className="text-white">
                 Isoler son logement,
@@ -24,7 +29,7 @@ const BlogContent = () => {
             </a>
           </div>
         </div>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-4 gap-y-4">
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-x-4 gap-y-4">
           <div className="bg-logoOne rounded-xl relative overflow-hidden max-h-[250px]">
             <img
               src="https://images.unsplash.com/photo-1595877244574-e90ce41ce089?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"
@@ -32,7 +37,30 @@ const BlogContent = () => {
               className="object-cover pointer-events-none w-full"
             />
             <div className="absolute inset-0 bg-gradient-to-t from-black opacity-50"></div>
-            <div className="absolute bottom-10 left-10 text-xl lg:text-2xl">
+            <div className="absolute bottom-10 left-10 text-lg lg:text-xl">
+              <a href="#" className="text-white">
+                <h3 className="text-white">
+                  Choisir les finitions idéales travaux
+                  <br /> pour son logement après les
+                  <br /> travaux
+                </h3>
+                <a
+                  href="#"
+                  className="text-sm font-normal leading-6 text-white"
+                >
+                  En savoir plus <span aria-hidden="true">→</span>
+                </a>
+              </a>
+            </div>
+          </div>
+          <div className="bg-logoOne rounded-xl relative overflow-hidden max-h-[250px]">
+            <img
+              src="https://images.unsplash.com/photo-1595877244574-e90ce41ce089?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80"
+              alt="isolation des combles"
+              className="object-cover pointer-events-none w-full"
+            />
+            <div className="absolute inset-0 bg-gradient-to-t from-black opacity-50"></div>
+            <div className="absolute bottom-10 left-10 text-lg lg:text-xl">
               <a href="#" className="text-white">
                 <h3 className="text-white">
                   Choisir les finitions idéales travaux
@@ -55,7 +83,7 @@ const BlogContent = () => {
               className="object-cover pointer-events-none w-full object-center"
             />
             <div className="absolute inset-0 bg-gradient-to-t from-black opacity-50"></div>
-            <div className="absolute bottom-10 left-10 text-xl lg:text-2xl">
+            <div className="absolute bottom-10 left-10 text-lg lg:text-xl">
               <a href="#" className="text-white">
                 <h3 className="text-white">
                   Les différents type de charpente,
