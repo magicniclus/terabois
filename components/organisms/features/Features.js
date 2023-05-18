f107e0f0dee2a4a02c71ffc69ce7fae75bbe3134
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-18 10:19:47 +0200

    Update Features.js

--- components/organisms/features/Features.js
+++ components/organisms/features/Features.js
@@ -27,28 +27,27 @@ const prestations = [
     description:
       "Nous intervenons rapidement pour tout dépannage informatique.",
     href: "#",
-    icon: "https://images.unsplash.com/photo-1603439810849-5e013dc3ce73?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1476&q=80",
+    icon: "https://images.unsplash.com/photo-1624409990662-03f19cc0e0cc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80",
   },
 ];
 
 const Features = () => {
   return (
-    <div className="bg-white py-10">
+    <div className="bg-white px-10 pt-3">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1">
+        <div className="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 gap-x-10">
           {prestations.map((prestation) => (
-            <div className="mt:-5 grid grid-cols-1 gap-y-20 gap-x-6 sm:gap-x-8 w-full mx-auto hover:bg-gray-50 p-7 rounded-xl">
+            <div className="mt:-5 grid grid-cols-1 gap-y-20 gap-x-6 sm:gap-x-8 w-full mx-auto p-4 hover:bg-gray-50 rounded-xl">
               <div className="group relative flex flex-col justify-between">
-                <div className="aspect-w-3 aspect-h-2 rounded-lg overflow-hidden sm:aspect-w-3 sm:aspect-h-4 relative">
+                <div className="aspect-w-3 aspect-h-2 rounded-lg overflow-hidden sm:aspect-w-3 sm:aspect-h-4 relative h-48">
                   <img
                     src={prestation.icon}
                     alt={prestation.name}
                     className="object-cover pointer-events-none w-full h-full"
                   />
-
                   <div className="absolute inset-0 bg-gradient-to-t from-black opacity-50"></div>
                 </div>
-                <h3 className="mt-6 text-lg text-gray-800">
+                <h3 className="mt-6 text-lg text-gray-800 p-2">
                   <a href={prestation.href}>
                     <span className="absolute inset-0" />
                     {prestation.name}
@@ -56,7 +55,7 @@ const Features = () => {
                 </h3>
                 <a
                   href="#"
-                  className="text-sm font-normal leading-6 text-gray-800"
+                  className="text-sm font-normal leading-6 text-gray-800 p-2"
                 >
                   En savoir plus <span aria-hidden="true">→</span>
                 </a>
