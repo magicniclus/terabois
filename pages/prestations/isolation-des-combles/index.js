549cf96a7ed1657213463c32eb18bae96710faea
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-19 08:11:51 +0200

    Create index.js

---
+++ pages/prestations/isolation-des-combles/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Blog from "../../../layouts/Blog";
+import FormContent from "../../../components/organisms/formContent/FormContent";
+
+const index = () => {
+  return (
+    <Blog>
+      <FormContent />
+    </Blog>
+  );
+};
+
+export default index;
