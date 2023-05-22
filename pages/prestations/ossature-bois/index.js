d2df198053ba1d6109b065541cedb4d021eacdf6
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-22 16:17:55 +0200

    Update index.js

---
+++ pages/prestations/ossature-bois/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Blog from "../../../layouts/Blog";
+import FormContent from "../../../components/organisms/formContent/FormContent";
+
+const index = () => {
+  return (
+    <Blog>
+      <FormContent
+        description="Avec Terabois, donnez vie à vos projets de bardage, extension ou agrandissement en bois, alliant esthétique et durabilité. Nous vous garantissons un travail de qualité pour un habitat qui vous ressemble."
+        avantages={[
+          "L'entreprise n°1 en Gironde pour les extensions bois",
+          "Travaillez avec nos charpentier reconnus",
+          "Plus de 90% de nos clients satisfaits",
+          "Entreprise certifiée RGE Qualibat",
+        ]}
+        title="Ossature bois"
+        prestations={[
+          "Surélévation bois",
+          "Agrandissement bois",
+          "Bardage bois",
+        ]}
+        image="ossature.jpeg"
+      />
+    </Blog>
+  );
+};
+
+export default index;
