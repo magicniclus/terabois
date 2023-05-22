e4732364f33f911a32f458df9c07b8455e06fd08
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-22 16:17:57 +0200

    Update index.js

--- pages/prestations/terrasse-bois/index.js
+++ pages/prestations/terrasse-bois/index.js
@@ -1,7 +1,24 @@
 import React from "react";
+import Blog from "../../../layouts/Blog";
+import FormContent from "../../../components/organisms/formContent/FormContent";
 
 const index = () => {
-  return <div></div>;
+  return (
+    <Blog>
+      <FormContent
+        title="Terrasse bois"
+        description="Confiez à Terabois la réalisation de votre terrasse en bois pour un espace extérieur chaleureux et durable. Nos experts garantissent une finition impeccable pour un véritable coin de paradis."
+        avantages={[
+          "L'entreprise n°1 en Gironde pour les terrasses bois",
+          "Travaillez avec nos charpentier reconnus",
+          "Plus de 90% de nos clients satisfaits",
+          "Entreprise certifiée RGE Qualibat",
+        ]}
+        prestations={["Terrasse bois"]}
+        image="terrasse.jpeg"
+      />
+    </Blog>
+  );
 };
 
 export default index;
