9d391bfe8740e854d9049fcacf0124f456424dab
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-22 16:17:43 +0200

    Create BadgeAide.js

---
+++ components/organisms/badges/BadgeAide.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const BadgeAide = () => {
+  return (
+    <div className="flex absolute -bottom-8 left-10 p-3 bg-white rounded-lg shadow-xl">
+      <img src="/img/logos/logoRGE.png" className="h-10 w-10 rounded-full" />
+      <img src="/img/logos/LOGO-CEE.png" className="h-10 w-10 rounded-full" />
+      <img
+        src="/img/logos/maprimerenov.png"
+        className="h-10 w-10 rounded-full"
+      />
+      <img src="/img/logos/synerciel.png" className="h-10 w-10 rounded-full" />
+    </div>
+  );
+};
+
+export default BadgeAide;
