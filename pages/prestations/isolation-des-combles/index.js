17fc18272fa10c3b644d15d2010cc1e8ac6d118c
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-22 16:17:51 +0200

    Update index.js

--- pages/prestations/isolation-des-combles/index.js
+++ pages/prestations/isolation-des-combles/index.js
@@ -5,7 +5,22 @@ import FormContent from "../../../components/organisms/formContent/FormContent";
 const index = () => {
   return (
     <Blog>
-      <FormContent />
+      <FormContent
+        description="Choisir Terabois expert en isolation durable, pour un confort optimal et des économies d'énergie. Profitez des aides de l'État pour un investissement malin et éco-responsable."
+        avantages={[
+          "L'entreprise n°1 en Gironde pour l'isolation des combles",
+          "Jusqu'à 80% d'aide de l'état sur vos travaux d'isolation",
+          "Plus de 90% de nos clients satisfaits",
+          "Entreprise certifiée RGE Qualibat",
+        ]}
+        prestations={[
+          "isolation des combles perdues",
+          "Isolation sous rampant",
+          "Isolation sous toiture",
+        ]}
+        image="isolation.jpeg"
+        badge={true}
+      />
     </Blog>
   );
 };
