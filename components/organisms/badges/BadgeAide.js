2bfacba06c8bbf9f483d82c6555f98ebde9d188b
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-24 10:51:19 +0200

    Update BadgeAide.js

--- components/organisms/badges/BadgeAide.js
+++ components/organisms/badges/BadgeAide.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const BadgeAide = () => {
   return (
-    <div className="flex absolute -bottom-8 left-40 p-3 bg-white rounded-lg shadow-xl">
+    <div className="flex p-3 rounded-lg w-max mt-10">
       <img src="/img/logos/logoRGE.png" className="h-10 w-10 rounded-full" />
       <img src="/img/logos/LOGO-CEE.png" className="h-10 w-10 rounded-full" />
       <img
