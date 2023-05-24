b76d1411773be056eea2555e93323751e56d5676
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-24 13:18:04 +0200

    Create Global.js

---
+++ layouts/Global.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Head from "next/head";
+import Footer from "../components/organisms/footer/Footer";
+import GlobalNav from "../components/organisms/nav/GlobalNav";
+
+const Global = (props) => {
+  const description = props.description || null;
+  const title = props.title || null;
+  const canonical = props.canonical || "";
+  const robots = props.robots || "follow";
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+        <meta name="robots" content={"index, " + robots} />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="language" content="fr" />
+        <link rel="canonical" href={"https://www.terabois.fr/" + canonical} />
+      </Head>
+      <GlobalNav />
+      <main className="bg-white min-h-[100vh] relative">{props.children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Global;
