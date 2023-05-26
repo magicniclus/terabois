e0e6dd34e19359f89b460d40b1646f51a37c63d1
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-26 18:49:56 +0200

    Update getContent.js

--- utils/getContent.js
+++ utils/getContent.js
@@ -1,34 +1,38 @@
-import { CheckBadgeIcon } from "@heroicons/react/20/solid";
+import { CheckIcon } from "@heroicons/react/20/solid";
 
 export const getContent = (content) => {
   switch (content.tag) {
     case "h2":
       return (
-        <h2 className="mt-2 text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl">
+        <h2 className="text-justify text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl  mt-16">
           {content.content}
         </h2>
       );
     case "h3":
       return (
-        <h3 className="text-lg lg:text-xl font-semibold text-gray-800">
+        <h3 className="text-justify text-lg lg:text-xl font-semibold text-gray-800  mt-12">
           {content.content}
         </h3>
       );
     case "h4":
       return (
-        <h4 className="mt-16 text-normal font-semibold tracking-tight text-gray-800">
+        <h4 className="text-justify mt-16 text-normal font-semibold tracking-tight text-gray-800">
           {content.content}
         </h4>
       );
     case "p":
-      return <p className="text-nomral font-normal">{content.content}</p>;
+      return (
+        <p className="text-justify text-nomral font-normal mt-5">
+          {content.content}
+        </p>
+      );
     case "ul":
       return (
         <ul className="text-normal font-semibold">
           {content.content.map((listItem) => (
-            <div className="flex">
-              <CheckBadgeIcon
-                className="mt-1 h-5 w-5 flex-none text-blue"
+            <div className="flex mt-5">
+              <CheckIcon
+                className="mt-1 h-5 w-5 mr-2 flex-none text-blue"
                 aria-hidden="true"
               />
               <li
