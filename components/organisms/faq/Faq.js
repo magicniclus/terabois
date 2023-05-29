7a6772f1dc8e225b295fc1d44da4278edbb6f7e5
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-29 17:17:17 +0200

    Update Faq.js

--- components/organisms/faq/Faq.js
+++ components/organisms/faq/Faq.js
@@ -4,66 +4,110 @@ import { MinusSmallIcon, PlusSmallIcon } from "@heroicons/react/24/outline";
 
 const faqs = [
   {
-    question: "What's the best thing about Switzerland?",
+    question: "Quels services Terabois offre-t-il ?",
     answer:
-      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+      "Terabois est spécialisé dans plusieurs domaines dont l'isolation des combles et des murs, la réalisation de terrasses en bois, les travaux de charpente et de couverture, et la construction d'ossature bois.",
   },
   {
-    question: "What's the best thing about Switzerland?",
+    question: "Où se situe Terabois ?",
     answer:
-      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+      "Terabois est situé en Gironde. Cependant, nous couvrons une large zone autour de cette région, veuillez nous contacter pour confirmer la couverture de votre emplacement.",
   },
   {
-    question: "What's the best thing about Switzerland?",
+    question: "Quelle est l'expérience de l'équipe de Terabois ?",
     answer:
-      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+      "Le créateur de Terabois a plus de 30 ans d'expérience dans le domaine et a réalisé plus de 3000 chantiers. Notre équipe est composée de professionnels qualifiés et expérimentés dans chaque service que nous offrons.",
   },
   {
-    question: "What's the best thing about Switzerland?",
+    question: "Quels types de matériaux utilisez-vous pour vos services ?",
     answer:
-      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+      "Nous utilisons principalement du bois pour nos constructions et nos travaux d'isolation. Cependant, nous nous adaptons en fonction des spécificités de chaque projet. N'hésitez pas à nous contacter pour discuter de vos besoins spécifiques.",
   },
   {
-    question: "What's the best thing about Switzerland?",
+    question: "Proposez-vous des devis gratuits ?",
     answer:
-      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+      "Oui, nous offrons des devis gratuits. Vous pouvez nous contacter par téléphone ou par e-mail, ou remplir le formulaire de contact sur notre site web.",
   },
   {
-    question: "What's the best thing about Switzerland?",
+    question: "Quel est le délai moyen pour un chantier ?",
     answer:
-      "I don't know, but the flag is a big plus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas cupiditate laboriosam fugiat.",
+      "Le délai dépend de la taille et de la complexité du projet. Une fois que nous aurons discuté de vos besoins spécifiques, nous serons en mesure de vous fournir une estimation plus précise.",
+  },
+  {
+    question:
+      "Vos travaux d'isolation sont-ils éligibles aux crédits d'impôts ?",
+    answer:
+      "Oui, certains de nos travaux d'isolation peuvent être éligibles aux crédits d'impôts pour la transition énergétique. Nous pouvons vous fournir plus d'informations à ce sujet lors de la discussion de votre projet.",
+  },
+  {
+    question: "Quelle est la durée de vie moyenne d'une terrasse en bois ?",
+    answer:
+      "La durée de vie d'une terrasse en bois dépend de nombreux facteurs, dont le type de bois utilisé et l'entretien régulier. En général, une terrasse bien entretenue peut durer entre 15 et 25 ans.",
+  },
+  {
+    question: "Proposez-vous des garanties sur vos travaux ?",
+    answer:
+      "Oui, tous nos travaux sont sous garantis décénale. Nous fournirons des détails spécifiques lors de la rédaction du devis.",
+  },
+  {
+    question: "Terabois est-il assuré et agréé ?",
+    answer:
+      "Oui, Terabois est pleinement assuré et agréé (RGE qualibat etc... ). Nous respectons toutes les réglementations en vigueur dans notre domaine.",
+  },
+  {
+    question:
+      "Quel type de maintenance requiert une structure en ossature bois ?",
+    answer:
+      "Une structure en ossature bois nécessite un entretien minimal, principalement lié à la protection contre les insectes et l'humidité. Un contrôle régulier et des traitements préventifs sont recommandés.",
+  },
+  {
+    question: "Pouvez-vous travailler sur des bâtiments historiques ?",
+    answer:
+      "Oui, nous avons l'expérience et les compétences nécessaires pour travailler sur des bâtiments historiques. Nous respectons les réglementations et les contraintes spécifiques à ces types de projets afin de préserver l'intégrité architecturale tout en assurant une rénovation de qualité.",
   },
 ];
 
 const Faq = () => {
   return (
     <div className="bg-white">
-      <div className="mx-auto max-w-7xl px-6 py-16 sm:py-24 lg:px-8">
-        <h2 className="text-2xl font-bold leading-10 tracking-tight text-gray-900">
-          Frequently asked questions
-        </h2>
-        <p className="mt-6 max-w-2xl text-base leading-7 text-gray-600">
-          Have a different question and can’t find the answer you’re looking
-          for? Reach out to our support team by{" "}
-          <a
-            href="#"
-            className="font-semibold text-indigo-600 hover:text-indigo-500"
-          >
-            sending us an email
-          </a>{" "}
-          and we’ll get back to you as soon as we can.
-        </p>
-        <div className="mt-20">
-          <dl className="space-y-16 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-16 sm:space-y-0 lg:grid-cols-3 lg:gap-x-10">
+      <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8 lg:py-40">
+        <div className="mx-auto max-w-4xl divide-y divide-gray-900/10">
+          <h1 className="text-2xl font-bold leading-10 tracking-tight text-gray-900">
+            Questions fréquentes
+          </h1>
+          <dl className="mt-10 space-y-6 divide-y divide-gray-900/10">
             {faqs.map((faq) => (
-              <div key={faq.id}>
-                <dt className="text-base font-semibold leading-7 text-gray-900">
-                  {faq.question}
-                </dt>
-                <dd className="mt-2 text-base leading-7 text-gray-600">
-                  {faq.answer}
-                </dd>
-              </div>
+              <Disclosure as="div" key={faq.question} className="pt-6">
+                {({ open }) => (
+                  <>
+                    <dt>
+                      <Disclosure.Button className="flex w-full items-start justify-between text-left text-gray-900">
+                        <span className="text-base font-semibold leading-7">
+                          {faq.question}
+                        </span>
+                        <span className="ml-6 flex h-7 items-center">
+                          {open ? (
+                            <MinusSmallIcon
+                              className="h-6 w-6"
+                              aria-hidden="true"
+                            />
+                          ) : (
+                            <PlusSmallIcon
+                              className="h-6 w-6"
+                              aria-hidden="true"
+                            />
+                          )}
+                        </span>
+                      </Disclosure.Button>
+                    </dt>
+                    <Disclosure.Panel as="dd" className="mt-2 pr-12">
+                      <p className="text-base leading-7 text-gray-600">
+                        {faq.answer}
+                      </p>
+                    </Disclosure.Panel>
+                  </>
+                )}
+              </Disclosure>
             ))}
           </dl>
         </div>
