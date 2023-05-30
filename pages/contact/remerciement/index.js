396a43469dfb5b5a6ec443cd0324b58c4849b8ce
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-30 10:11:15 +0200

    Create index.js

---
+++ pages/contact/remerciement/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Global from "../../../layouts/Global";
+import Remerciement from "../../../components/organisms/content/Remerciement";
+const index = () => {
+  return (
+    <Global>
+      <Remerciement />
+    </Global>
+  );
+};
+
+export default index;
