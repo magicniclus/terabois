a748046095ba49e6895226e0b0fe67327804ca42
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-30 14:51:02 +0200

    Create index.js

---
+++ pages/prestations/isolation-des-murs-exterieurs/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Blog from "../../../layouts/Blog";
+import FormContent from "../../../components/organisms/formContent/FormContent";
+
+const index = () => {
+  return (
+    <Blog>
+      <FormContent
+        title="Isolation des murs exterieurs"
+        description="Terabois garantit une isolation intérieure et extérieure de qualité pour plus de confort et d'économies d'énergie. Bénéficiez des aides de l'État pour un habitat performant et écologique."
+        avantages={[
+          "L'entreprise n°1 en Gironde pour l'isolation des murs exterieurs",
+          "Jusqu'à 80% d'aide de l'état sur vos travaux d'isolation",
+          "Plus de 90% de nos clients satisfaits sur +3000 réalisations",
+          "Entreprise certifiée RGE Qualibat",
+        ]}
+        prestations={["Isolation des murs exterieurs (ITE)"]}
+        image="ite.jpeg"
+        badge={true}
+        badgeChantier={false}
+      />
+    </Blog>
+  );
+};
+
+export default index;
