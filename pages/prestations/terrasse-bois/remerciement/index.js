4b765ae8ecc0c3edcc7aa66a6ebecc32780fdbf7
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-30 14:51:23 +0200

    Create index.js

---
+++ pages/prestations/terrasse-bois/remerciement/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Global from "../../../../layouts/Global";
+import Remerciement from "../../../../components/organisms/content/Remerciement";
+
+const index = () => {
+  return (
+    <Global>
+      <Remerciement />
+    </Global>
+  );
+};
+
+export default index;
