056ea53490006fd8d32347fccf2801b25fab1e7b
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-30 20:45:54 +0200

    Update Nav.js

--- components/organisms/nav/Nav.js
+++ components/organisms/nav/Nav.js
@@ -277,7 +277,7 @@ const Nav = (props) => {
         <div className="fixed inset-0 z-100" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-800/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5 py-7">
+            <a href="/" className="-m-1.5 p-1.5 py-7">
               <span className="sr-only">Terabois</span>
               <img
                 className="h-8 w-auto"
@@ -374,7 +374,7 @@ const Nav = (props) => {
               </div>
               <div className="py-6">
                 <a
-                  href="#"
+                  href="/connexion"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   Se connecter
