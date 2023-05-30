e65ae237dd20d13298799661709592f40625dd52
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-30 11:11:00 +0200

    Create index.js

---
+++ pages/politique-de-confidentialite/index.js
@@ -0,0 +1,112 @@
+import React from "react";
+import Global from "../../layouts/Global";
+
+const index = () => {
+  return (
+    <Global>
+      <div className="mx-auto max-w-7xl  px-6 lg:px-8 pb-10">
+        <h1 className="text-3xl font-bold tracking-tight text-gray-800 sm:text-4xl  mt-16">
+          Politique de confidentialité
+        </h1>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl  mt-16">
+          Introduction
+        </h2>
+        <p className="text-nomral font-normal mt-5">
+          Bienvenue sur TeraBois. Nous nous engageons à protéger votre vie
+          privée et à respecter les normes de protection des données
+          personnelles en vigueur.
+        </p>
+        <p className="text-nomral font-normal mt-5">
+          La présente politique de confidentialité a pour but de vous informer
+          de nos pratiques en matière de collecte, d'utilisation, de divulgation
+          et de protection des informations que vous nous fournissez via notre
+          site web. Veuillez lire attentivement cette politique de
+          confidentialité pour comprendre nos pratiques concernant vos
+          informations personnelles.
+        </p>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl  mt-16">
+          Collecte des informations
+        </h2>
+        <p className="text-nomral font-normal mt-5">
+          TeraBois collecte des informations que vous nous fournissez lorsque
+          vous remplissez des formulaires sur notre site web, par exemple
+          lorsque vous demandez un devis pour une rénovation. Les informations
+          collectées peuvent inclure votre nom, adresse e-mail, numéro de
+          téléphone, adresse et autres détails relatifs à votre projet de
+          rénovation.
+        </p>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl  mt-16">
+          Utilisation des informations
+        </h2>
+        <p className="text-nomral font-normal mt-5">
+          TeraBois utilise les informations recueillies pour vous fournir des
+          devis précis, répondre à vos questions, communiquer avec vous au sujet
+          de nos services, et améliorer nos offres et services.
+        </p>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl  mt-16">
+          Partage des informations
+        </h2>
+        <p className="text-nomral font-normal mt-5">
+          TeraBois est une entreprise qui récolte des leads pour son propre
+          compte et ne partage, ne vend, ni n'échange vos informations
+          personnelles avec des tiers, sauf si la loi l'exige, dans le cadre
+          d'une transaction d'entreprise, ou si nous avons obtenu votre
+          consentement préalable.
+        </p>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl  mt-16">
+          Sécurité des informations
+        </h2>
+        <p className="text-nomral font-normal mt-5">
+          TeraBois utilise des mesures de sécurité appropriées pour protéger
+          contre la perte, l'abus et l'altération des données utilisées par
+          notre système. Cependant, aucune transmission de données sur Internet
+          n'est 100% sécurisée, et bien que nous nous efforçons de protéger vos
+          informations personnelles, nous ne pouvons garantir leur sécurité
+          absolue.
+        </p>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl  mt-16">
+          Vos droits
+        </h2>
+        <p className="text-nomral font-normal mt-5">
+          Conformément aux lois en vigueur, vous avez le droit d'accéder à vos
+          informations personnelles, de les rectifier, de demander leur
+          suppression, de vous opposer à leur traitement pour des motifs
+          légitimes, et de demander la limitation du traitement.
+        </p>
+        <p className="text-nomral font-normal mt-5">
+          Pour exercer ces droits, ou pour toute question ou préoccupation
+          concernant cette politique de confidentialité, veuillez nous contacter
+          à [adresse e-mail de l'entreprise].
+        </p>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl  mt-16">
+          Changements à cette politique de confidentialité
+        </h2>
+        <p className="text-nomral font-normal mt-5">
+          TeraBois se réserve le droit de modifier cette politique de
+          confidentialité à tout moment. Toutes les modifications seront
+          publiées sur cette page, avec la date de la dernière mise à jour.
+          Veuillez consulter régulièrement cette politique de confidentialité
+          pour être informé de toute modification.
+        </p>
+        <p className="text-nomral font-normal mt-5">
+          Dernière mise à jour : 30/05/2023
+        </p>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl  mt-16">
+          Consentement
+        </h2>
+        <p className="text-nomral font-normal mt-5">
+          En utilisant notre site web, vous consentez à la collecte, à
+          l'utilisation et au partage de vos informations tel que décrit dans
+          cette politique de confidentialité. Si vous n'êtes pas d'accord avec
+          cette politique, veuillez ne pas utiliser notre site web.
+        </p>
+        <p className="text-nomral font-normal mt-5">
+          Pour toute question relative à notre politique de confidentialité,
+          n'hésitez pas à nous contacter à [adresse e-mail de l'entreprise].
+        </p>
+      </div>
+    </Global>
+  );
+};
+
+export default index;
