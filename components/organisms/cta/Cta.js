2ed1e48adbae7b916d1dae694e719d2a18739120
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-30 12:26:42 +0200

    Update Cta.js

--- components/organisms/cta/Cta.js
+++ components/organisms/cta/Cta.js
@@ -11,12 +11,15 @@ const Cta = () => {
         </h2>
         <div className="mt-10 flex items-center gap-x-6 lg:mt-0 lg:flex-shrink-0">
           <a
-            href="#"
+            href="/contact"
             className="rounded-md bg-blue px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blueClear"
           >
             Prendre rendez-vous
           </a>
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-800">
+          <a
+            href="/#prestations"
+            className="text-sm font-semibold leading-6 text-gray-800"
+          >
             Nos préstations <span aria-hidden="true">→</span>
           </a>
         </div>
