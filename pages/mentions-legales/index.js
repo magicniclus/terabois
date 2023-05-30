739267f8cf2652892f2a9102ff270e005473f534
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-30 11:10:58 +0200

    Create index.js

---
+++ pages/mentions-legales/index.js
@@ -0,0 +1,94 @@
+import React from "react";
+import Global from "../../layouts/Global";
+
+const index = () => {
+  return (
+    <Global>
+      <div className="mx-auto max-w-7xl  px-6 lg:px-8 pb-10">
+        <h1 className="text-3xl font-bold tracking-tight text-gray-800 sm:text-4xl  mt-16">
+          Mentions Légales de TeraBois
+        </h1>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl  mt-16">
+          Informations Générales
+        </h2>
+        <p className="text-nomral font-normal mt-5">
+          Le présent site web, accessible à l'adresse [URL du site web de
+          l'entreprise], est la propriété de TeraBois, une entreprise
+          spécialisée dans la rénovation de l'habitat basée à Bordeaux.
+        </p>
+        <p className="text-nomral font-normal mt-5">
+          TeraBois est une [forme juridique de l'entreprise] dont le siège
+          social est situé à [adresse du siège social de l'entreprise], inscrite
+          au registre du commerce et des sociétés de [ville] sous le numéro
+          [numéro d'inscription].
+        </p>
+        <p className="text-nomral font-normal mt-5">
+          Téléphone : [numéro de téléphone de l'entreprise] <br />
+          Email : [adresse e-mail de l'entreprise]
+        </p>
+        <p className="text-nomral font-normal mt-5">
+          Directeur de la publication : [nom du directeur de la publication]
+        </p>
+        <p className="text-nomral font-normal mt-5">
+          Le site est hébergé par [nom de l'hébergeur], dont le siège social est
+          situé à [adresse du siège social de l'hébergeur].
+        </p>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl  mt-16">
+          Propriété Intellectuelle
+        </h2>
+        <p className="text-nomral font-normal mt-5">
+          Tous les éléments du site web de TeraBois, y compris les textes,
+          graphiques, logos, images, photographies, vidéos, sons, sont la
+          propriété exclusive de TeraBois, sauf indication contraire. Ces
+          éléments sont protégés par les lois françaises et les textes
+          internationaux relatifs au respect des droits d'auteur et de la
+          propriété intellectuelle.
+        </p>
+        <p className="text-nomral font-normal mt-5">
+          La reproduction et l'utilisation de ces éléments ne sont autorisées
+          qu'à des fins d'information pour un usage personnel et privé. Toute
+          autre utilisation est expressément interdite sans l'autorisation
+          préalable écrite de TeraBois.
+        </p>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl  mt-16">
+          Limitation de responsabilité
+        </h2>
+        <p className="text-nomral font-normal mt-5">
+          TeraBois s'efforce d'assurer au mieux de ses possibilités l'exactitude
+          et la mise à jour des informations diffusées sur son site. Toutefois,
+          TeraBois ne peut garantir l'exactitude, la précision ou l'exhaustivité
+          des informations disponibles sur ce site.
+        </p>
+        <p className="text-nomral font-normal mt-5">
+          TeraBois n'est pas responsable des éventuelles erreurs, d'une absence
+          de disponibilité des informations, ainsi que de la présence de virus
+          sur son site. En aucun cas, TeraBois ne peut être tenu responsable des
+          dommages directs ou indirects résultant de l'utilisation de son site
+          web.
+        </p>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl  mt-16">
+          Modification des mentions légales
+        </h2>
+        <p className="text-nomral font-normal mt-5">
+          TeraBois se réserve le droit de modifier les présentes mentions
+          légales à tout moment. L'utilisateur est donc invité à les consulter
+          régulièrement.
+        </p>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl  mt-16">
+          Droit applicable
+        </h2>
+        <p className="text-nomral font-normal mt-5">
+          Les présentes mentions légales sont régies par le droit français. En
+          cas de litige et à défaut d'accord amiable, le litige sera porté
+          devant les tribunaux français conformément aux règles de compétence en
+          vigueur.
+        </p>
+        <p className="text-nomral font-normal mt-5">
+          Date de dernière mise à jour : 23/05/2023
+        </p>
+      </div>
+    </Global>
+  );
+};
+
+export default index;
