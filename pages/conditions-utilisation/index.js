437b79436739333d781425a9bfae5370aada3360
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-30 11:10:56 +0200

    Create index.js

---
+++ pages/conditions-utilisation/index.js
@@ -0,0 +1,81 @@
+import Global from "../../layouts/Global";
+
+const index = () => {
+  return (
+    <Global>
+      <div className="mx-auto max-w-7xl  px-6 lg:px-8 pb-10">
+        <h1 className="text-3xl font-bold tracking-tight text-gray-800 sm:text-4xl  mt-16">
+          Conditions d'Utilisation de TeraBois
+        </h1>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl  mt-16">
+          1. Acceptation des Conditions d'Utilisation
+        </h2>
+        <p className="text-nomral font-normal mt-5">
+          L'accès et l'utilisation du site [URL du site web de l'entreprise],
+          ci-après désigné "le site", sont soumis aux présentes "Conditions
+          d'Utilisation" détaillées ci-dessous ainsi qu'aux lois et/ou
+          règlements applicables. La connexion et l'accès à ce site impliquent
+          l'acceptation intégrale et sans réserve de l'internaute à toutes les
+          dispositions des présentes Conditions d'Utilisation.
+        </p>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl  mt-16">
+          2. Utilisation du Site
+        </h2>
+        <p className="text-nomral font-normal mt-5">
+          Le site est accessible gratuitement à tout utilisateur disposant d'un
+          accès à internet. Tous les coûts afférents à l'accès au site, que ce
+          soient les frais matériels, logiciels ou d'accès à internet sont
+          exclusivement à la charge de l'utilisateur.
+        </p>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl  mt-16">
+          3. Propriété Intellectuelle
+        </h2>
+        <p className="text-nomral font-normal mt-5">
+          Le contenu du site, y compris mais sans s'y limiter, les textes,
+          graphiques, images, logos, icônes, et le code informatique, est la
+          propriété exclusive de TeraBois, sauf indication contraire, et est
+          protégé par le droit d'auteur, le droit des marques et d'autres lois
+          applicables. Toute reproduction ou représentation partielle ou totale
+          de ce site ou de tout ou partie des éléments se trouvant sur ce site
+          est strictement interdite sans l'autorisation écrite de TeraBois.
+        </p>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl  mt-16">
+          4. Limitation de Responsabilité
+        </h2>
+        <p className="text-nomral font-normal mt-5">
+          TeraBois s'efforce d'assurer au mieux de ses possibilités l'exactitude
+          et la mise à jour des informations diffusées sur son site. Toutefois,
+          TeraBois ne peut garantir l'exactitude, la précision ou l'exhaustivité
+          des informations disponibles sur ce site.
+        </p>
+        <p className="text-nomral font-normal mt-5">
+          TeraBois n'est pas responsable des éventuelles erreurs, d'une absence
+          de disponibilité des informations, ainsi que de la présence de virus
+          sur son site. En aucun cas, TeraBois ne peut être tenu responsable des
+          dommages directs ou indirects résultant de l'utilisation de son site
+          web.
+        </p>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl  mt-16">
+          5. Modification des Conditions d'Utilisation
+        </h2>
+        <p className="text-nomral font-normal mt-5">
+          TeraBois se réserve le droit de modifier les présentes conditions
+          d'utilisation à tout moment. L'utilisateur est donc invité à les
+          consulter régulièrement.
+        </p>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-800 sm:text-3xl  mt-16">
+          6. Droit Applicable
+        </h2>
+        <p className="text-nomral font-normal mt-5">
+          Les présentes conditions d'utilisation sont régies par le droit
+          français. En cas de litige et à défaut d'accord amiable, le litige
+          sera porté devant les tribunaux français conformément aux règles de
+          compétence en vigueur.
+        </p>
+        <p className="text-nomral font-normal mt-5">
+          Date de dernière mise à jour : 30/05/2023
+        </p>
+      </div>
+    </Global>
+  );
+};
