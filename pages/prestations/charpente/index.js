9de6ebb98efd07697c56d437eef6d6803ef2ab43
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-05-30 14:50:52 +0200

    Create index.js

---
+++ pages/prestations/charpente/index.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Blog from "../../../layouts/Blog";
+import FormContent from "../../../components/organisms/formContent/FormContent";
+
+const index = () => {
+  return (
+    <Blog>
+      <FormContent
+        title="Charpentier en gironde"
+        description="TeraBois est votre partenaire de confiance pour des charpentes robustes et de qualité supérieure. Nous combinons techniques traditionnelles et innovations modernes pour assurer une structure solide et durable à votre maison. Choisissez TeraBois pour une habitation alliant stabilité, performance et esthétisme."
+        avantages={[
+          "L'entreprise n°1 en Gironde en charpente",
+          "Plus de 90% de nos clients satisfaits sur +3000 réalisations",
+          "Entreprise certifiée RGE Qualibat",
+        ]}
+        prestations={[
+          "Charpente traditionnelle",
+          "Charpente américaine",
+          "Ossature bois",
+          "Traitement bois",
+        ]}
+        image="ite.jpeg"
+        badge={true}
+        badgeChantier={false}
+      />
+    </Blog>
+  );
+};
+
+export default index;
