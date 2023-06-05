f98d8f54275c06dc0a721184367d569309c0e41f
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-05 14:51:49 +0200

    Update index.js

--- pages/prestations/isolation-des-murs-interieurs/index.js
+++ pages/prestations/isolation-des-murs-interieurs/index.js
@@ -11,14 +11,14 @@ const index = () => {
     >
       <FormContent
         title="Isolation des murs interieurs"
-        description="Terabois garantit une isolation intérieure et extérieure de qualité pour plus de confort et d'économies d'énergie. Bénéficiez des aides de l'État pour un habitat performant et écologique."
+        description="Terabois garantit une isolation des murs intérieurs et extérieurs de qualité pour plus de confort et d'économies d'énergie. Bénéficiez des aides de l'État pour un habitat performant et écologique."
         avantages={[
-          "L'entreprise n°1 en Gironde pour l'isolation des murs exterieurs",
+          "L'entreprise n°1 en Gironde pour l'isolation des murs intérieurs",
           "Jusqu'à 80% d'aide de l'état sur vos travaux d'isolation",
           "Plus de 90% de nos clients satisfaits sur +3000 réalisations",
           "Entreprise certifiée RGE Qualibat",
         ]}
-        prestations={["Isolation des murs exterieurs (ITE)"]}
+        prestations={["Isolation des murs interieurs (ITI)"]}
         image="/img/prestations/iti.jpeg"
         alt="Isolation des murs interieurs"
         badge={true}
