9c0bb4df822d45721b3eb4ce52b1699cd8079cde
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-05 12:50:42 +0200

    Update index.js

--- pages/prestations/charpente/remerciement/index.js
+++ pages/prestations/charpente/remerciement/index.js
@@ -5,7 +5,11 @@ import Remerciement from "../../../../components/organisms/content/Remerciement"
 const index = () => {
   return (
     <Global>
-      <Remerciement />
+      <Remerciement
+        title="Merci pour Votre RDV chez Terabois, Spécialiste de l'Habitat"
+        description="Merci d'avoir pris RDV avec Terabois. Expert en rénovation, nous avons hâte de travailler avec vous pour améliorer votre habitat à Bordeaux et en Gironde."
+        tags="RDV, Merci, Rénovation Habitat, Terabois, Isolation, Couverture, Charpente, Terrasse bois, Extension ossature, Bordeaux, Gironde"
+      />
     </Global>
   );
 };
