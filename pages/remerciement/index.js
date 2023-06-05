ea99dd80125b7b454a63b8b31be571915e144f9b
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-05 12:51:04 +0200

    Update index.js

--- pages/remerciement/index.js
+++ pages/remerciement/index.js
@@ -4,7 +4,11 @@ import Remerciement from "../../components/organisms/content/Remerciement";
 
 const index = () => {
   return (
-    <Global title="" description="" tags="">
+    <Global
+      title="Merci pour Votre RDV chez Terabois, Spécialiste de l'Habitat"
+      description="Merci d'avoir pris RDV avec Terabois. Expert en rénovation, nous avons hâte de travailler avec vous pour améliorer votre habitat à Bordeaux et en Gironde."
+      tags="RDV, Merci, Rénovation Habitat, Terabois, Isolation, Couverture, Charpente, Terrasse bois, Extension ossature, Bordeaux, Gironde"
+    >
       <Remerciement />
     </Global>
   );
