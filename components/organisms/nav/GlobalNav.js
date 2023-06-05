e8ef69d081e7914579336c49dccb51b486afc3e3
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-05 19:57:33 +0200

    Update GlobalNav.js

--- components/organisms/nav/GlobalNav.js
+++ components/organisms/nav/GlobalNav.js
@@ -223,7 +223,7 @@ const GlobalNav = () => {
                   >
                     <div className="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
                       <item.icon
-                        className="h-6 w-6 text-gray-600 group-hover:text-indigo-600"
+                        className="h-6 w-6 text-gray-600 group-hover:text-blue"
                         aria-hidden="true"
                       />
                     </div>
