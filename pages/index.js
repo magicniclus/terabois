c46c7a4fbdaf07c4340381169765cea09c12c274
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-05 11:43:46 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,11 @@ import BlogContent from "../components/organisms/content/BlogContent";
 
 export default function Home() {
   return (
-    <Basic>
+    <Basic
+      title="Terabois Bordeaux : Isolation & Rénovation Maison en Gironde"
+      description="Chez Terabois, optimisez votre confort avec nos services d'isolation des combles, murs et toitures, et découvrez nos offres de rénovation et d'extensions en ossature bois à Bordeaux et en Gironde."
+      tags="Isolation des combles, Isolation sous toiture, Isolation des murs, Couverture, Charpente, Terrasse en bois, Extension d'ossature, Rénovation de l'habitat, Bordeaux, Gironde"
+    >
       <Features />
       <BlogContent />
       <Testamonials />
