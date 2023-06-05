e86abe999422ef14e7973ea72e83947f151dfe75
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-05 11:43:44 +0200

    Update index.js

--- pages/faq/index.js
+++ pages/faq/index.js
@@ -4,7 +4,11 @@ import Faq from "../../components/organisms/faq/Faq";
 import Cta from "../../components/organisms/cta/Cta";
 const index = () => {
   return (
-    <Global>
+    <Global
+      title="FAQ Terabois : Réponses à vos Questions sur l'Isolation et la Rénovation"
+      description="Découvrez notre FAQ pour toutes vos questions sur les services d'isolation et de rénovation de Terabois à Bordeaux et en Gironde. Informez-vous ici."
+      tags="FAQ Terabois, Isolation, Rénovation, Questions, Couverture, Charpente, Terrasse en bois, Extension ossature, Bordeaux, Gironde"
+    >
       <Faq />
       <Cta />
     </Global>
