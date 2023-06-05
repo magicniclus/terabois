5baab3357a428f8814d8aaea1bcfecc1ee52a247
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-06-05 18:46:58 +0200

    Update index.js

--- pages/prestations/nettoyage-de-couverture/index.js
+++ pages/prestations/nettoyage-de-couverture/index.js
@@ -11,7 +11,7 @@ const index = () => {
     >
       <FormContent
         title="Nettoyage de couverture en gironde"
-        description="eraBois est votre spécialiste pour le nettoyage de couverture. Nous combinons des techniques de nettoyage efficaces et respectueuses pour préserver la qualité et prolonger la durabilité de votre couverture. Faites confiance à TeraBois pour un nettoyage de couverture minutieux qui redonne à votre toiture toute son esthétique et sa performance"
+        description="TeraBois est votre spécialiste pour le nettoyage de couverture. Nous combinons des techniques de nettoyage efficaces et respectueuses pour préserver la qualité et prolonger la durabilité de votre couverture. Faites confiance à TeraBois pour un nettoyage de couverture minutieux qui redonne à votre toiture toute son esthétique et sa performance"
         avantages={[
           "L'entreprise n°1 en Gironde en nettoyage de couverture",
           "Plus de 90% de nos clients satisfaits sur +3000 réalisations",
