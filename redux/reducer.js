c545987bd22e96241d0a2a3862db9e248535e648
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-07-14 13:21:11 +0200

    Update reducer.js

--- redux/reducer.js
+++ redux/reducer.js
@@ -1,5 +1,14 @@
 const initState = {
   postId: "",
+  user: null,
+  openSearch: false,
+  dataDisplayManagment: {
+    page: 1,
+    allData: [],
+  },
+  userAdmin: {
+    name: "",
+  },
 };
 
 const reducer = (state = initState, action) => {
@@ -9,6 +18,40 @@ const reducer = (state = initState, action) => {
         ...state,
         postId: action.payload,
       };
+    case "SET_DATA":
+      return {
+        ...state,
+        dataDisplayManagment: {
+          ...state.dataDisplayManagment,
+          numberOfPages: Math.ceil(action.payload.length / 10),
+          numberOfDatas: action.payload.length,
+        },
+      };
+
+    case "SET_PAGE":
+      return {
+        ...state,
+        dataDisplayManagment: {
+          ...state.dataDisplayManagment,
+          page: action.payload,
+        },
+      };
+
+    case "OPEN_SEARCH":
+      return {
+        ...state,
+        openSearch: !state.openSearch,
+      };
+
+    case "SET_USER_NAME":
+      return {
+        ...state,
+        userAdmin: {
+          ...state.userAdmin,
+          name: action.payload,
+        },
+      };
+
     default:
       return state;
   }
