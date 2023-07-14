3be32d108c0c9797105c3fbd0a9adda441b817c1
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-07-14 13:21:04 +0200

    Update firebase.config.js

--- firebase/firebase.config.js
+++ firebase/firebase.config.js
@@ -1,8 +1,10 @@
-import { initializeApp, getApps } from "firebase/app";
+import { initializeApp, getApps, getApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
+// Configuration de Firebase pour terabois
+const teraboisConfig = {
   apiKey: "AIzaSyCotU40WMv8MymTSJLFe7PPKN9AEV12Q30",
   authDomain: "terabois-275d5.firebaseapp.com",
   databaseURL:
@@ -13,16 +15,42 @@ const firebaseConfig = {
   appId: "1:696961840881:web:f1113dc97592775bfcdb50",
 };
 
-// Initialize Firebase
-let app;
+// Configuration de Firebase pour maprimerenov
+const maprimerenovConfig = {
+  apiKey: "AIzaSyANjQ3xCyjveltI081x5ceT_Xv4cUylsCc",
+  authDomain: "maprimerenov-58fdf.firebaseapp.com",
+  databaseURL:
+    "https://maprimerenov-58fdf-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "maprimerenov-58fdf",
+  storageBucket: "maprimerenov-58fdf.appspot.com",
+  messagingSenderId: "855231284792",
+  appId: "1:855231284792:web:e491151c0617175c01c8cd",
+};
 
-// Vérifie si une application Firebase existe déjà
-if (!getApps().length) {
-  app = initializeApp(firebaseConfig);
-} else {
-  app = getApps()[0]; // Utilise l'application Firebase existante
+let teraboisApp;
+let maprimerenovApp;
+
+if (typeof window !== "undefined") {
+  if (!getApps().length) {
+    teraboisApp = initializeApp(teraboisConfig, "Terabois");
+    maprimerenovApp = initializeApp(maprimerenovConfig, "Maprimerenov");
+  } else {
+    teraboisApp = getApp("Terabois");
+    maprimerenovApp = getApp("Maprimerenov");
+  }
 }
 
-const database = getDatabase(app);
+const teraboisDatabase = teraboisApp ? getDatabase(teraboisApp) : null;
+const maprimerenovDatabase = maprimerenovApp
+  ? getFirestore(maprimerenovApp)
+  : null;
 
-export { database };
+const maprimerenovAuth = maprimerenovApp ? getAuth(maprimerenovApp) : null;
+const teraboisAuth = teraboisApp ? getAuth(teraboisApp) : null;
+
+export {
+  maprimerenovDatabase,
+  teraboisDatabase,
+  maprimerenovAuth,
+  teraboisAuth,
+};
