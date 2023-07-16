8d884c18a490d0879ffc7a9805821ac9e087324d
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-07-16 12:15:01 +0200

    Update getData.js

--- firebase/getData.js
+++ firebase/getData.js
@@ -1,4 +1,11 @@
-import { collection, getDocs } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  doc,
+  query,
+  where,
+  updateDoc,
+} from "firebase/firestore";
 import { maprimerenovDatabase } from "./firebase.config";
 
 export const getData = async () => {
@@ -11,6 +18,64 @@ export const getData = async () => {
   try {
     const querySnapshot = await getDocs(docRef);
     return querySnapshot.docs.map((doc) => doc.data());
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const getDataByPhone = async (phone) => {
+  if (!maprimerenovDatabase) {
+    throw new Error("Firebase not initialized");
+  }
+
+  const docRef = collection(maprimerenovDatabase, "primeRenovProspect");
+
+  try {
+    const q = query(docRef, where("phone", "==", phone));
+    const querySnapshot = await getDocs(q);
+
+    const data = querySnapshot.docs.map((doc) => {
+      const docData = doc.data();
+
+      if (docData.createAt && typeof docData.createAt.toDate === "function") {
+        docData.createAt = docData.createAt.toDate().toISOString();
+      }
+
+      if (docData.date && typeof docData.date.toDate === "function") {
+        docData.date = docData.date.toDate().toISOString();
+      }
+
+      return docData;
+    });
+
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+export const updateCommentByPhone = async (phone, newComment) => {
+  if (!maprimerenovDatabase) {
+    throw new Error("Firebase not initialized");
+  }
+
+  const docRef = collection(maprimerenovDatabase, "primeRenovProspect");
+  const q = query(docRef, where("phone", "==", phone));
+
+  try {
+    const querySnapshot = await getDocs(q);
+    if (!querySnapshot.empty) {
+      const docId = querySnapshot.docs[0].id;
+      const docToUpdate = doc(
+        maprimerenovDatabase,
+        "primeRenovProspect",
+        docId
+      );
+      await updateDoc(docToUpdate, { comment: newComment });
+      console.log("Phone number updated successfully");
+    } else {
+      console.log("No document with the specified date found");
+    }
   } catch (err) {
     console.error(err);
   }
