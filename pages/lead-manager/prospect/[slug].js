bade93c2dedb3a3265ffd98d6cc17e95823c3779
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-07-16 12:15:06 +0200

    Update [slug].js

--- pages/lead-manager/prospect/[slug].js
+++ pages/lead-manager/prospect/[slug].js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
+import BackOffice from "../../../layouts/BackOffice";
+import { getDataByPhone } from "../../../firebase/getData";
+import { useRouter } from "next/router";
+import ProspectCntent from "../../../components/organisms/content/ProspectCntent";
 
-const Prospect = ({ id }) => {
-  return <div></div>;
+const Prospect = ({ data }) => {
+  const user = data[0];
+
+  return (
+    <BackOffice userName={user.name}>
+      <ProspectCntent data={user} />
+    </BackOffice>
+  );
 };
+
+export async function getServerSideProps(context) {
+  const id = context.params.slug; // Extraire l'ID du chemin
+  const data = await getDataByPhone(id);
+
+  return {
+    props: {
+      data: data || [], // Retourner un tableau vide si les données sont indéfinies
+    },
+  };
+}
 
 export default Prospect;
