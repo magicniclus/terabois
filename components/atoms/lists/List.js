209434f843eb8ba164b8f49d319f2e5d472f0c81
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-07-18 15:30:01 +0200

    Update List.js

--- components/atoms/lists/List.js
+++ components/atoms/lists/List.js
@@ -1,8 +1,6 @@
 import React from "react";
 
-const List = (props) => {
-  const options = props.options;
-  const status = props.status || "select";
+const List = ({ options, status, onChange }) => {
   return (
     <div className="flex">
       <select
@@ -10,6 +8,7 @@ const List = (props) => {
         name="status"
         className="mt-2 block w-content rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6"
         defaultValue={status}
+        onChange={(e) => onChange(e.target.value)}
       >
         {options.map((option, idx) => (
           <option key={idx}>{option}</option>
